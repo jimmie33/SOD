7f9c4d92f35436ecf63d589388a2c503d565fba2
jimmie33 <jimmie33@gmail.com>
2016-04-07 18:12:10 -0400

    Adding lines to create folders

--- code/util/downloadModel.m
+++ code/util/downloadModel.m
@@ -2,10 +2,16 @@ function downloadModel(modelName)
 
 if strcmp(modelName, 'GoogleNet')
     fprintf('downloading GoogleNet model files\n'); 
+    if ~exist(fullfile('model',modelName), 'dir')
+        mkdir(fullfile('model',modelName));
+    end
     urlwrite('model', 'model/GoogleNet/deploy.prototxt');
     urlwrite('model', 'model/GoogleNet/GoogleNet_SOD_finetune.caffemodel');
 elseif strcmp(modelName, 'VGG16')
     fprintf('downloading VGG16 model files\n'); 
+    if ~exist(fullfile('model',modelName), 'dir')
+        mkdir(fullfile('model',modelName));
+    end
     urlwrite('model', 'model/VGG16/deploy.prototxt');
     urlwrite('model', 'model/VGG16/VGG16_SOD_finetune.caffemodel');
 else
