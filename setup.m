6170ab8fdeec0c112f38fb19e8eacb9191b496f5
jimmie33 <jimmie33@gmail.com>
2016-04-09 01:37:55 -0400

    changed the default model to GoogleNet

--- setup.m
+++ setup.m
@@ -5,7 +5,7 @@ addpath(genpath('./'))
 
 % default: using GoogleNet
 % other option: VGG16 which is used in the paper
-param = getParam('VGG16');  
+param = getParam('GoogleNet');  
 % param = getParam('VGG16');
 
 net = initModel(param);
